32aaf7e6d70c3143d18903168b03f357d3755306
ab2075@xpluto <ab2075@cam.ac.uk>
2020-06-29 11:29:52 +0100

    rebasing

--- src/useMyRec.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const myRec_1 = require("./myRec");
-var r1 = new myRec_1.myRec(2, 10);
-var v1 = r1.area;
-console.log("myrec2", v1);
-//# sourceMappingURL=useMyRec.js.map
